185dd6bc8ba82e51c2c206fd3e4ba2d9ad22426d
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-02-14 08:51:50 +0000

    Simplify frontend assets bundle

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,6 +7,7 @@ import uglify from 'gulp-uglify'
 import minifyCSS from 'gulp-minify-css'
 import minifyHTML from 'gulp-minify-html'
 import rev from 'gulp-rev'
+import concatCss from 'gulp-concat-css'
 
 import browserify from 'browserify'
 import babelify from 'babelify'
@@ -14,43 +15,21 @@ import source from 'vinyl-source-stream'
 import buffer from 'vinyl-buffer'
 
 import yaml from 'js-yaml'
-import path from 'path'
 import fs from 'fs'
 import del from 'del'
 
-
 let paths = {
   scripts: [
-    'bower_components/jquery/dist/jquery.js',
-    'bower_components/bootstrap/dist/js/bootstrap.js',
-    'bower_components/history.js/scripts/bundled-uncompressed/html5/jquery.history.js',
-    'bower_components/underscore/underscore.js',
-    'bower_components/requirejs/require.js',
-    'bower_components/eventEmitter/EventEmitter.js',
-    'bower_components/bootstrap-filestyle/src/bootstrap-filestyle.js',
-    'bower_components/jquery-form/jquery.form.js',
-    'bower_components/query-string/query-string.js',
-    'bower_components/parsleyjs/dist/parsley.js',
-    'bower_components/markdown-it/dist/markdown-it.js',
-    'bower_components/moment/moment.js',
-    'bower_components/eonasdan-bootstrap-datetimepicker/src/js/bootstrap-datetimepicker.js'
-  ],
-  app_scripts: [
     'scripts/themis.js'
   ],
   fonts: [
-    'bower_components/bootstrap/dist/fonts/*.eot',
-    'bower_components/bootstrap/dist/fonts/*.svg',
-    'bower_components/bootstrap/dist/fonts/*.ttf',
-    'bower_components/bootstrap/dist/fonts/*.woff',
-    'bower_components/bootstrap/dist/fonts/*.woff2'
+    'node_modules/bootstrap/dist/fonts/*.eot',
+    'node_modules/bootstrap/dist/fonts/*.svg',
+    'node_modules/bootstrap/dist/fonts/*.ttf',
+    'node_modules/bootstrap/dist/fonts/*.woff',
+    'node_modules/bootstrap/dist/fonts/*.woff2'
   ],
   stylesheets: [
-    'bower_components/bootstrap/dist/css/bootstrap.css',
-    'bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css',
-    'bower_components/animate.css/animate.css'
-  ],
-  app_stylesheets: [
     'stylesheets/themis.sass'
   ],
   html: [
@@ -58,21 +37,17 @@ let paths = {
   ]
 }
 
-
-function isProduction() {
-  return (process.env['NODE_ENV'] === 'production')
+function isProduction () {
+  return process.env['NODE_ENV'] === 'production'
 }
-
 
 gulp.task('clean_scripts', (callback) => {
   del(['public/assets/js/*'], callback)
 })
 
-
-gulp.task('app_scripts', ['clean_scripts'], () => {
-
+gulp.task('scripts', ['clean_scripts'], () => {
   return browserify({
-    entries: paths.app_scripts,
+    entries: paths.scripts,
     extensions: ['.js'],
     debug: !isProduction()
   })
@@ -89,30 +64,18 @@ gulp.task('app_scripts', ['clean_scripts'], () => {
     .pipe(gulp.dest('public/assets/js'))
 })
 
-
-function isSass(file) {
-  return (path.extname(file.path) === '.sass')
-}
-
-
 gulp.task('clean_stylesheets', (callback) => {
   del(['public/assets/css/*'], callback)
 })
 
-
 gulp.task('stylesheets', ['clean_stylesheets'], () => {
   return gulp
     .src(paths.stylesheets)
-    .pipe(gulpIf(isProduction, minifyCSS()))
-    .pipe(gulp.dest('public/assets/css'))
-})
-
-
-gulp.task('app_stylesheets', ['clean_stylesheets'], () => {
-  return gulp
-    .src(paths.app_stylesheets)
     .pipe(sass({ indentedSyntax: true, errLogToConsole: true }))
     .pipe(gulpIf(isProduction, minifyCSS()))
+    .pipe(concatCss('themis.css', {
+      rebaseUrls: false
+    }))
     .pipe(gulp.dest('public/assets/css'))
     .pipe(rev())
     .pipe(gulp.dest('public/assets/css'))
@@ -120,11 +83,9 @@ gulp.task('app_stylesheets', ['clean_stylesheets'], () => {
     .pipe(gulp.dest('public/assets/css'))
 })
 
-
 gulp.task('clean_fonts', (callback) => {
   del(['public/assets/fonts/*'], callback)
 })
-
 
 gulp.task('fonts', ['clean_fonts'], () => {
   gulp
@@ -132,13 +93,11 @@ gulp.task('fonts', ['clean_fonts'], () => {
     .pipe(gulp.dest('public/assets/fonts'))
 })
 
-
 gulp.task('clean_html', (callback) => {
   del(['public/html/*'], callback)
 })
 
-
-gulp.task('html', ['clean_html', 'stylesheets', 'fonts', 'app_scripts', 'app_stylesheets'], () => {
+gulp.task('html', ['clean_html', 'fonts', 'scripts', 'stylesheets'], () => {
   let opts = null
 
   try {
@@ -152,7 +111,7 @@ gulp.task('html', ['clean_html', 'stylesheets', 'fonts', 'app_scripts', 'app_sty
         css: cachebusting_css['themis.css']
       }
     }
-  } catch(e) {
+  } catch (e) {
     console.log(e)
     opts = {}
   }
@@ -163,7 +122,6 @@ gulp.task('html', ['clean_html', 'stylesheets', 'fonts', 'app_scripts', 'app_sty
     .pipe(gulpIf(isProduction, minifyHTML()))
     .pipe(gulp.dest('public/html'))
 })
-
 
 gulp.task('default', ['html'])
 
